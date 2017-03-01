4d5adff66abe2d1274f0e5874be64364ba2b8bfe
ariadna <gomezariadna.ag@gmail.com>
2017-03-01 22:23:38 +0000

    milestone 3 completed

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 // JavaScript File
 $(document).ready(function(){
+
+
+    $("button").click(function(){
+        var person = $("#password").val();
+        
+        if (person === "candy") {
+            $("div").append("CONGRATULATIONS,YOU GUESSED IT!!!");
+        } 
+    });
+});    
     
     
     
-    
-})
